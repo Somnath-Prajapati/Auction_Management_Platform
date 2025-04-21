4150cd5980b9d3777de79148303b488d7bae6bf0
Somnath-Prajapati <somnath.prajapati@neosoftmail.com>
2025-04-21 17:21:19 +0530

    entities ref added

--- Backend/AuctionManagementSystem/AuctionManagementSystem.Domain/Entities/TblStaticPagesSetting.cs
+++ Backend/AuctionManagementSystem/AuctionManagementSystem.Domain/Entities/TblStaticPagesSetting.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace AuctionManagementSystem.Persistence.TempEntities;
+namespace AuctionManagementSystem.Domain.Entities;
 
 public partial class TblStaticPagesSetting
 {
