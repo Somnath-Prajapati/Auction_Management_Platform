ada98f48c934d0e095dfe4449d661313f7895421
AffanDhanse2001 <affan.dhanse@neosoftmail.com>
2025-04-21 17:15:30 +0530

    UserInterface

---
+++ Backend/AuctionManagementSystem/AuctionManagementSystem.Application/Contracts/IUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AuctionManagementSystem.Persistence.TempEntities;
+
+namespace AuctionManagementSystem.Application.Contracts
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<TblUser>> GetAllUsersAsync();
+        Task<TblUser> GetUserById(int id);
+        Task<TblUser> CreateUserAsync(TblUser tblUser);
+        Task<bool> UpdateUserAsync(int id, TblUser tblUser);
+        Task<bool> DeleteUserAsync(int id);
+
+    }
+}
