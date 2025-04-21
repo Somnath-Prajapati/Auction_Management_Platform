e18736a8fd0e7c86fffe5eaea300fb889d02af29
AffanDhanse2001 <affan.dhanse@neosoftmail.com>
2025-04-21 16:20:23 +0530

    Added Frontend and Backend boilerPlate

---
+++ Backend/AuctionManagementSystem/AuctionManagementSystem.Api/Program.cs
@@ -0,0 +1,39 @@
+using AuctionManagementSystem.Persistence;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AuctionManagementSystem.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            //builder.Services.AddPersistanceServices(builder.Services, builder.Configuration);
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddOpenApi();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
