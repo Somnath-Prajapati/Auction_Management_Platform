1dfb57f3cdc0d5ad8c350d71037d3937cef93419
Auction project <vaishnavi.patil@neosoftmail.com>
2025-04-25 18:27:42 +0530

    uploaded the request backend

---
+++ Backend/AuctionManagementSystem/AuctionManagementSystem.Application/Features/Requests/Command/UpdRequest/UdpRequestCommandHandler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AuctionManagementSystem.Application.Contracts.Request;
+using AuctionManagementSystem.Domain.Entities.Request;
+using MediatR;
+
+namespace AuctionManagementSystem.Application.Features.Requests.Command.UpdRequest
+{
+    public class UdpRequestCommandHandler : IRequestHandler<UdpRequestCommand, TblRequest>
+    {
+        private readonly IRequestRepository _requestRepository;
+
+        public UdpRequestCommandHandler(IRequestRepository requestRepository)
+        {
+            _requestRepository = requestRepository;
+        }
+
+        public async Task<TblRequest> Handle(UdpRequestCommand request, CancellationToken cancellationToken)
+        {
+            var existingRequest = await _requestRepository.GetRequestByIdQuery(request.RequestId);
+            if (existingRequest == null)
+                return null;
+
+            existingRequest.RequestNumber = request.RequestNumber;
+            existingRequest.UserId = request.UserId;
+            existingRequest.Username = request.Username;
+            existingRequest.MobileNumber = request.MobileNumber;
+            existingRequest.Email = request.Email;
+            existingRequest.RequestTypeId = request.RequestTypeId;
+            existingRequest.AssetId = request.AssetId;
+            existingRequest.TransactionId = request.TransactionId;
+            existingRequest.RequestDateTime = request.RequestDateTime;
+            existingRequest.RequestStatusId = request.RequestStatusId;
+            existingRequest.CustomerNote = request.CustomerNote;
+            existingRequest.AdminNote = request.AdminNote;
+            existingRequest.CreatedByAdmin = request.CreatedByAdmin;
+            existingRequest.UpdatedOn = System.DateTime.Now;
+
+            await _requestRepository.UpdateRequest(existingRequest);
+            return existingRequest;
+        }
+    }
+}
---
+++ Backend/AuctionManagementSystem/AuctionManagementSystem.Application/Features/Requests/Queries/GetAllRequests/GetAllRequestQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AuctionManagementSystem.Application.Contracts.Request;
+using AuctionManagementSystem.Application.Dtos.RequestsDtos;
+using AuctionManagementSystem.Domain.Entities.Request;
+using AutoMapper;
+using MediatR;
+
+namespace AuctionManagementSystem.Application.Features.Requests.Queries.GetAllRequests
+{
+    public class GetAllRequestQueryHandler : IRequestHandler<GetAllRequestQuery, IEnumerable<RequestDto>>
+    {
+        private readonly IRequestRepository _requestRepository;
+        private readonly IMapper _mapper;
+
+        public GetAllRequestQueryHandler(IRequestRepository requestRepository,IMapper mapper)
+        {
+            _requestRepository = requestRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<RequestDto>> Handle(GetAllRequestQuery request, CancellationToken cancellationToken)
+
+        {
+            var requests = await _requestRepository.GetAllRequestQuery();
+            return _mapper.Map<IEnumerable<RequestDto>>(requests);
+        }
+    }
+}
