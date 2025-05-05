1d54bcf63b6efc3338ad2b2c0807b1e412fccd4c
AffanDhanse2001 <affan.dhanse@neosoftmail.com>
2025-05-05 18:02:59 +0530

    non

--- Backend/AuctionManagementSystem/AuctionManagementSystem.Api/Program.cs
+++ Backend/AuctionManagementSystem/AuctionManagementSystem.Api/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using AuctionManagementSystem.Application.Contracts;
+using AuctionManagementSystem.Api.Middleware;
 
 namespace AuctionManagementSystem.Api
 {
