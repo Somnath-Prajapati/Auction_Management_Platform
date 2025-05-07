dc306311abc1d981c41ce0cd9eeb3afc487409f8
Prathmesh <prathmeshaghao@gmail.com>
2025-05-07 18:55:12 +0530

    minor

--- Backend/AuctionManagementSystem/AuctionManagementSystem.Application/Dtos/Assets/UpdateAssetCategoryDto.cs
+++ Backend/AuctionManagementSystem/AuctionManagementSystem.Application/Dtos/Assets/UpdateAssetCategoryDto.cs
@@ -2,9 +2,9 @@
 
 namespace AuctionManagementSystem.Application.Dtos.Assets
 {
-    public class CreateAssetCategoryDto
+    public class UpdateAssetCategoryDto
     {
-        public string CategoryName { get; set; }
+        public string? CategoryName { get; set; }
         public string? Subcategory { get; set; }
         public decimal DepositPercentage { get; set; }
         public string? Details { get; set; }
@@ -13,9 +13,9 @@ namespace AuctionManagementSystem.Application.Dtos.Assets
         public decimal BuyerCommission { get; set; }
         public DateTime RegistrationDeadline { get; set; }
         public string? Icon { get; set; }
-        public IFormFile? IconFile { get; set; } // To receive uploaded file from form
-        public IFormFile? Document { get; set; } // To receive the documents
+        public IFormFile? IconFile { get; set; } // actual uploaded file
 
+        public IFormFile? Document { get; set; } //to upload the files/documents
         public decimal? Vatpercentage { get; set; }
         public int StatusId { get; set; }
         public int? Vatid { get; set; }
