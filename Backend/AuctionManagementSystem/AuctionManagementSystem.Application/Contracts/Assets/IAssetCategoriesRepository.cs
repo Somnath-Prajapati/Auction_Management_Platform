50bfd478787cb99db3acec07fa73ab8493bd2fc2
Prathmesh <prathmeshaghao@gmail.com>
2025-05-07 11:11:50 +0530

    updated Auction

---
+++ Backend/AuctionManagementSystem/AuctionManagementSystem.Application/Contracts/Assets/IAssetCategoriesRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AuctionManagementSystem.Domain.Entities.Asset;
+
+namespace AuctionManagementSystem.Application.Contracts.Assets
+{
+    public interface IAssetCategoriesRepository
+    {
+        Task<List<TblAssetCategory>> GetAllAsync();
+
+        Task<TblAssetCategory> AddAsync(TblAssetCategory tblAssetCategory);
+    }
+}
