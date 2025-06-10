b57a461617107b3c032272643fd5fce5a6bc5218
Prathmesh <prathmeshaghao@gmail.com>
2025-06-10 15:53:32 +0530

    translate

---
+++ Backend/AuctionManagementSystem/AuctionManagementSystem.Application/Dtos/Translations/AssetTranslationDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionManagementSystem.Application.Dtos.Translations
+{
+    public class AssetTranslationDto
+    {
+        public int Id { get; set; }
+        public int AssetId { get; set; }
+        public int LanguageId { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? SalesNotes { get; set; }
+    }
+
+}
