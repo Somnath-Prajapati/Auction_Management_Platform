0d5da5c85c9899d8e3564f2b655e1687bf042a6c
Prathmesh <prathmeshaghao@gmail.com>
2025-06-10 15:53:32 +0530

    translate

---
+++ Backend/AuctionManagementSystem/AuctionManagementSystem.Application/Dtos/Translations/CreateAuctionCategoryTranslationDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionManagementSystem.Application.Dtos.Translations
+{
+    public class CreateAuctionCategoryTranslationDto
+    {
+        public int CategoryId { get; set; }
+        public int LanguageId { get; set; }
+        public string TranslatedName { get; set; }
+    }
+}
